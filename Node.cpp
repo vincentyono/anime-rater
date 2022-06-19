c3e38e6adcad917f67a19e19d6c1d4892dbec0c0
Vincent Yono <vincentyono@protonmail.com>
2022-06-19 14:31:03 +0700

    add Node.h implementation

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,13 @@
 #include "Node.h"
+
+template <typename T> Node<T>::Node() { this->next = nullptr; }
+
+template <typename T> Node<T>::Node(T data) {
+  this->data = data;
+  this->next = nullptr;
+}
+
+template <typename T> Node<T>::Node(T data, Node<T> *next) {
+  this->data = data;
+  this->next = next;
+}
