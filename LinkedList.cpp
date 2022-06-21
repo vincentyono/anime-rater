31d1118402931a250b954fa644c6256bd81fbf35
Pandya Limawan <86063345+pan-dya@users.noreply.github.com>
2022-06-21 21:35:36 +0700

    added sort

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -27,6 +27,36 @@ T *LinkedList<T>::search(std::string value) override const {
   return nullptr;
 }
 
-template <typename T> void LinkedList<T>::sort() const {}
+template <typename T> int LinkedList<T>::getLength() const {
+    int count = 0;
+
+    Node<T> *current = this->head;
+    while(current != nullptr){
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+template <typename T> void LinkedList<T>::sort() const {
+Node<T>* current1 = this->head;
+    Node<T>* current2 = this->head;
+
+    for (int i = 0; i < getLength(); i++) {
+        for (int j = 0; j < getLength() - 1; j++) {
+            if (current1->data < current2->data) {
+                int temp = current1->data;
+                current1->data = current2->data;
+                current2->data = temp;
+
+            }
+            current2 = current2->next;
+        }
+            current2 = this->head;
+            current1 = this->head->next;
+            for (int k = 0; k < i; k++) {
+                current1 = current1->next;
+        }
+    }}
 
 template <typename T> void LinkedList<T>::printList() const {}
