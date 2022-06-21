1aae183771256f3dc12324157ac0aedaf24e0567
Pandya Limawan <86063345+pan-dya@users.noreply.github.com>
2022-06-21 21:34:55 +0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -13,6 +13,7 @@ public:
   void append(T data) override;
   T *search(T value) const override;
   void sort() const override;
+  int getLength() const override;
   void printList() const override;
   void remove(T value);
 };
